98f1577b1a93d511771d226642c4382405abc373
Kevishakha <kevishakha@gmail.com>
2024-12-02 11:53:10 +0530

    Add files via upload

---
+++ restaurant/assests/images/assests.jsx
@@ -0,0 +1,731 @@
+import React from "react";
+import bgimg from "./Image/bgimg.avif";
+import Biryani from "./Image/Biryani.avif";
+import Cake from "./Image/cake.avif";
+import Chinese from "./Image/Chinese.avif";
+import CholeBhatore from "./Image/chole bhature.avif";
+import Coffee from "./Image/coffee.avif";
+import Dessert from "./Image/Desserts.avif";
+import Dining from "./Image/Dining.avif";
+import GulabJamun from "./Image/Gulab jamun.avif";
+import Kebabs from "./Image/Kebabs.avif";
+import Khichdi from "./Image/Khichdi.avif";
+import Lassi from "./Image/lassi.avif";
+import Noodles from "./Image/Noodles.avif";
+import NorthIndian from "./Image/North Indian.avif";
+import Omelete from "./Image/Omelette.avif";
+import OrderOnline from "./Image/orderOnline.avif";
+import PaneerButter from "./Image/paneerbutter.avif";
+import Paratha from "./Image/Paratha.avif";
+import PavBhaji from "./Image/Pav Bhaji.avif";
+import Poori from "./Image/Poori.avif";
+import PureVeg from "./Image/Pure Veg.avif";
+import Rasgulla from "./Image/Rasgulla.avif";
+import Shreeji from "./Image/shriji.avif";
+import Tandoori from "./Image/tandoori.avif";
+import Tea from "./Image/tea.avif";
+import rooftop from "./Image/rooftop.avif";
+import rooftop2 from "./Image/rooftops.avif";
+import dinetbl from "./Image/Diningtbl.avif";
+import chair from "./Image/chairs.avif";
+import groupChairs from "./Image/grouptbl.avif";
+import chef1 from "./Image/ranchef.jpeg";
+import chef2 from "./Image/ranChef2.jpeg";
+import chef3 from "./Image/ranOwner.jpeg";
+import caboutbg from "./Image/aboutbg.webp";
+import starter from "./Image/starter.webp";
+import Indian from "./Image/InChola.webp";
+import DessertF from "./Image/DesserSweet.webp";
+import Rice from "./Image/Rice.webp";
+import Salad from "./Image/Salad.avif";
+import Pasta from "./Image/Pasta.webp";
+import Pizza from "./Image/Pizzacorn.webp";
+import Nonveg from "./Image/NONVEG.webp";
+import Chapati from "./Image/ChapatiIn.webp";
+import SouthIndian from "./Image/MasalDoas.webp";
+import Soup from "./Image/Soup.webp";
+
+import starter1 from "./Image/starter.webp";
+import starter2 from "./Image/starter1.avif";
+import starter3 from "./Image/starter2.avif";
+import starter4 from "./Image/starter3.avif";
+import starter5 from "./Image/starter5.avif";
+import starter6 from "./Image/starter6.avif";
+
+import dessert1 from "./Image/Dessert.avif";
+import dessert2 from "./Image/Dessert1.avif";
+import dessert3 from "./Image/dessert2.avif";
+import dessert4 from "./Image/dessert3.avif";
+import dessert5 from "./Image/dessert4.avif";
+
+import salad1 from "./Image/Salad.avif";
+import salad2 from "./Image/salad1.avif";
+import salad3 from "./Image/saladCapsicum.avif";
+import salad4 from "./Image/saladveggies.avif";
+import salad5 from "./Image/Saladfruit.avif";
+
+import infood1 from "./Image/InChola.webp";
+import infood2 from "./Image/InPanner.webp";
+import infood3 from "./Image/InSBjiPoori.webp";
+
+import sIfood1 from "./Image/southIndian.webp";
+import sIfood2 from "./Image/idli.webp";
+import sIfood3 from "./Image/MasalDoas.webp";
+import sIfood4 from "./Image/paneerDosa.webp";
+
+import nVfood1 from "./Image/NonBiryani.webp";
+import nVfood2 from "./Image/NonCHicken.webp";
+import nVfood3 from "./Image/NonChickenSoup.webp";
+import nVfood4 from "./Image/NonFish.webp";
+import nVfood5 from "./Image/NonKabab.webp";
+import nVfood6 from "./Image/NonRoghanGosh.webp";
+import nVfood7 from "./Image/NONVEG.webp";
+
+import pasta1 from "./Image/Pasta.webp";
+import pasta2 from "./Image/pastawhitesauce.webp";
+import pasta3 from "./Image/pastaredSauce.webp";
+
+import pizza1 from "./Image/Pizza.webp";
+import pizza2 from "./Image/pizzaChilliCapsicum.webp";
+import pizza3 from "./Image/pizzapaneer.webp";
+import pizza4 from "./Image/Pizzacorn.webp";
+
+import soup1 from "./Image/vegSoup.webp";
+import soup2 from "./Image/Soup.webp";
+import soup3 from "./Image/NonChickenSoup.webp";
+
+import rice1 from "./Image/Rice.webp";
+import rice2 from "./Image/Ricepudding.avif";
+import rice3 from "./Image/RicePulav.avif";
+import rice4 from "./Image/NonBiryani.webp";
+
+
+import chapati1 from "./Image/ChapatiIn.webp";
+import chapati2 from "./Image/ParathaCHA.webp";
+import chapati3 from "./Image/NaanCha.webp";
+import chapati4 from "./Image/SpecFul.webp";
+import chapati5 from "./Image/SpeciaFull.webp";
+import chapati6 from "./Image/FullSpeecial.webp";
+import chapati7 from "./Image/FullThali.webp";
+
+
+import { IoMdAddCircle } from "react-icons/io";
+import { IoIosAdd } from "react-icons/io";
+import { IoMdRemove } from "react-icons/io";
+import { RxCross1 } from "react-icons/rx";
+import {FaCartPlus} from "react-icons/fa"
+import { CgProfile } from "react-icons/cg";
+
+import gal1 from './Image/gall1.avif'
+import gal2 from './Image/gall.avif'
+import gal3 from './Image/gall2.avif'
+import gal4 from './Image/gal3.avif'
+import gal5 from './Image/gal4.avif'
+import gal6 from './Image/gal5.avif'
+import gal7 from './Image/gal6.avif'
+import gal8 from './Image/Gallery.avif'
+
+
+export const Icon = {
+  addiconWhite: <IoMdAddCircle className="text-black bg-white" />,
+  addiconGreen: <IoIosAdd className="text-green bg-green " />,
+  removeiconRed: <IoMdRemove className="text-red bg-red " />,
+  loginpageClose: <RxCross1 />,
+  cartIcon:<FaCartPlus/>,
+  profileIcon:<CgProfile/>
+}
+
+export const addiconWhite = <IoMdAddCircle className="text-white" />
+export const addiconGreen = <IoIosAdd className="text-green" />
+export const removeiconRed = <IoIosAdd className="text-red" />
+
+
+export const gallery = {
+  rooftop,
+  rooftop2,
+  dinetbl,
+  chair,
+  groupChairs,
+  gal1,gal2,gal3,gal4,gal5,gal6,gal7,gal8,
+};
+
+export const chef = {
+  chef1,
+  chef2,
+  chef3,
+};
+
+export const Foodcategories = [
+  {
+    title: "Starter",
+    images: [starter],
+  },
+  {
+    title: "Dessert",
+    images: [DessertF],
+  },
+  {
+    title: "Salad",
+    images: [Salad],
+  },
+  {
+    title: "Indian-Food",
+    images: [Indian],
+  },
+  {
+    title: "South-Indian",
+    images: [SouthIndian],
+  },
+  {
+    title: "Nonveg",
+    images: [Nonveg],
+  },
+  {
+    title: "Pasta",
+    images: [Pasta],
+  },
+  {
+    title: "Pizza",
+    images: [Pizza],
+  },
+  {
+    title: "Soups",
+    images: [Soup],
+  },
+  {
+    title: "Rice",
+    images: [Rice],
+  },
+  {
+    title: "Chapati",
+    images: [Chapati],
+  },
+];
+
+export const foodList = [
+  {
+    id: "1",
+    name: "starter",
+    price: 30,
+    image: starter1,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "2",
+    name: "ice-cream",
+    price: 30,
+    image: starter2,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "3",
+    name: "roll-shakes",
+    price:  30,
+    image: starter3,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "4",
+    name: "starter",
+    price: 30,
+    image: starter4,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "5",
+    name: "starter",
+    price: 30,
+    image: starter5,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "6",
+    name: "starter",
+    price:30,
+    image: starter6,
+    description: "The awesome foode here",
+    category: "Starter",
+  },
+  {
+    id: "7",
+    name: "cake",
+    price: 50,
+    image: dessert1,
+    description: "The awesome dessert here",
+    category: "Dessert",
+  },
+  {
+    id: "8",
+    name: "dessert",
+    price: 50,
+    image: dessert2,
+    description: "The awesome dessert here",
+    category: "Dessert",
+  },
+  {
+    id: "9",
+    name: "Choco-cake",
+    price:50,
+    image: dessert3,
+    description: "The awesome dessert here",
+    category: "Dessert",
+  },
+  {
+    id: "10",
+    name: "dessert",
+    price: 50,
+    image: dessert4,
+    description: "The awesome dessert here",
+    category: "Dessert",
+  },
+  {
+    id: "11",
+    name: "dessert",
+    price: 50,
+    image: dessert5,
+    description: "The awesome dessert here",
+    category: "Dessert",
+  },
+  {
+    id: "12",
+    name: "fruit-salad",
+    price: 50,
+    image: salad1,
+    description: "The awesome salad here",
+    category: "Salad",
+  },
+  {
+    id: "13",
+    name: "fruit-salad",
+    price: 50,
+    image: salad2,
+    description: "The awesome salad here",
+    category: "Salad",
+  },
+  {
+    id: "14",
+    name: "salad",
+    price: 50,
+    image: salad3,
+    description: "The awesome salad here",
+    category: "Salad",
+  },
+  {
+    id: "15",
+    name: "vegie-salad",
+    price: 50,
+    image: salad4,
+    description: "The awesome salad here",
+    category: "Salad",
+  },
+  {
+    id: "16",
+    name: "fruit-salad",
+    price: 50,
+    image: salad5,
+    description: "The awesome salad here",
+    category: "Salad",
+  },
+  {
+    id: "17",
+    name: "Cholabhatora",
+    price: 50,
+    image: infood1,
+    description: "The awesome Indian-food here",
+    category: "Indian-Food",
+  },
+  {
+    id: "18",
+    name: "Shahi-Paneer",
+    price: 50,
+    image: infood2,
+    description: "The awesome Indian-food here",
+    category: "Indian-Food",
+  },
+  {
+    id: "19",
+    name: "Pori-sbji",
+    price: 50,
+    image: infood3,
+    description: "The awesome Indian-food here",
+    category: "Indian-Food",
+  },
+  {
+    id: "20",
+    name: "Masala Dosa",
+    price: 60,
+    image: sIfood1,
+    description: "The awesome south-indian food is here",
+    category: "South-Indian",
+  },
+  {
+    id: "21",
+    name: "Idli Sambhar",
+    price: 60,
+    image: sIfood2,
+    description: "The awesome south-indian food is here",
+    category: "South-Indian",
+  },
+  {
+    id: "22",
+    name: "Paper Dosa",
+    price: 60,
+    image: sIfood3,
+    description: "The awesome south-indian food is here",
+    category: "South-Indian",
+  },
+  {
+    id: "23",
+    name: "Paneer Dosa",
+    price: 60,
+    image: sIfood4,
+    description: "The awesome south-indian food is here",
+    category: "South-Indian",
+  },
+  {
+    id: "24",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood1,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "25",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood2,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "26",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood3,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "27",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood4,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "28",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood5,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "29",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood6,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "30",
+    name: "Nonveg",
+    price: 60,
+    image: nVfood7,
+    description: "The delicious food here",
+    category: "Nonveg",
+  },
+  {
+    id: "31",
+    name: "Pasta",
+    price: 50,
+    image: pasta1,
+    description: "The awesome pasta is here",
+    category: "Pasta",
+  },
+  {
+    id: "32",
+    name: "White Pasta",
+    price: 50,
+    image: pasta2,
+    description: "The awesome pasta is here",
+    category: "Pasta",
+  },
+  {
+    id: "33",
+    name: "Chilli Pasta",
+    price: 50,
+    image: pasta3,
+    description: "The awesome pasta is here",
+    category: "Pasta",
+  },
+  {
+    id: "34",
+    name: "Cheese Pizza",
+    price: 60 ,
+    image: pizza1,
+    description: "The awesome pizza is here",
+    category: "Pizza",
+  },
+  {
+    id: "35",
+    name: "Paneer Pizza",
+    price: 60 ,
+    image: pizza2,
+    description: "The awesome pizza is here",
+    category: "Pizza",
+  },
+  {
+    id: "36",
+    name: "CapsicumPizza",
+    price: 60 ,
+    image: pizza3,
+    description: "The awesome pizza is here",
+    category: "Pizza",
+  },
+  {
+    id: "37",
+    name: "Corn Pizza",
+    price: 60 ,
+    image: pizza4,
+    description: "The awesome pizza is here",
+    category: "Pizza",
+  },
+  {
+    id: "38",
+    name: "Tomato Soup",
+    price: 60 ,
+    image: soup1,
+    description: "The delicious soup is here",
+    category: "Soups",
+  },
+  {
+    id: "39",
+    name: "Veg Soup",
+    price: 30,
+    image: soup2,
+    description: "The delicious soup is here",
+    category: "Soups",
+  },
+  {
+    id: "40",
+    name: "Mix VegSoup",
+    price: 30,
+    image: soup3,
+    description: "The delicious soup is here",
+    category: "Soups",
+  },
+  {
+    id: "41",
+    name: "Pulav",
+    price: 30,
+    image: rice1,
+    description: "The awesome rice is here",
+    category: "Rice",
+  },
+  {
+    id: "42",
+    name: "Pudding",
+    price: 30,
+    image: rice2,
+    description: "The awesome rice is here",
+    category: "Rice",
+  },
+  {
+    id: "43",
+    name: "Biryani",
+    price: 30,
+    image: rice3,
+    description: "The awesome rice is here",
+    category: "Rice",
+  },
+  {
+    id: "44",
+    name: "ChickenBiryani",
+    price: 30,
+    image: rice4,
+    description: "The awesome rice is here",
+    category: "Rice",
+  },
+  {
+    id: "45",
+    name: "Chapati",
+    price: 5,
+    image: chapati1,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "46",
+    name: "Paratha",
+    price: 5,
+    image: chapati2,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "47",
+    name: "Naan",
+    price: 5,
+    image: chapati3,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "48",
+    name: "Special Thali",
+    price: 100,
+    image: chapati4,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "49",
+    name: "Special Thali",
+    price:100,
+    image: chapati5,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "50",
+    name: "Special Thali",
+    price: 100,
+    image: chapati6,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+  {
+    id: "51",
+    name: "Special Thali",
+    price:100,
+    image: chapati7,
+    description: "The awesome food is here",
+    category: "Chapati",
+  },
+
+
+  {
+    id: "52",
+    name: "Coffee",
+    price: 30,
+    image: Coffee,
+    description: "The awesome food is here",
+    category: "Soups",
+  },
+  {
+    id: "53",
+    name: "Tea",
+    price: 30,
+    image: Tea,
+    description: "The awesome food is here",
+    category: "Soups",
+  },
+  {
+    id: "54",
+    name: "Lassi",
+    price:30,
+    image: Lassi,
+    description: "The awesome food is here",
+    category: "Soups",
+  },
+  {
+    id: "55",
+    name: "GulabJamun",
+    price: 100,
+    image: GulabJamun,
+    description: "The awesome food is here",
+    category: "Dessert",
+  },
+  {
+    id: "56",
+    name: "Pav Bhaji",
+    price: 80,
+    image: PavBhaji,
+    description: "The awesome food is here",
+    category: "Indian-food",
+  },
+  {
+    id: "57",
+    name: "Omelete",
+    price: 30,
+    image: Omelete,
+    description: "The awesome food is here",
+    category: "Nonveg",
+  },
+  {
+    id: "58",
+    name: "Noodles",
+    price: 50,
+    image: Noodles,
+    description: "The awesome Chinese Noodles are here",
+    category: "Rice",
+  },
+
+];
+
+export const foodData = [
+  { id: 1, name: "", description: "", image: Biryani, price: 0 },
+  { id: 2, name: "", description: "", image: Cake, price: 0 },
+  { id: 3, name: "", description: "", image: Chinese, price: 0 },
+  { id: 4, name: "", description: "", image: CholeBhatore, price: 0 },
+  { id: 5, name: "", description: "", image: Coffee, price: 0 },
+  { id: 6, name: "", description: "", image: Dessert, price: 0 },
+  { id: 7, name: "", description: "", image: GulabJamun, price: 0 },
+  { id: 8, name: "", description: "", image: Kebabs, price: 0 },
+  { id: 9, name: "", description: "", image: Khichdi, price: 0 },
+  { id: 10, name: "", description: "", image: Lassi, price: 0 },
+  { id: 11, name: "", description: "", image: Noodles, price: 0 },
+  { id: 12, name: "", description: "", image: NorthIndian, price: 0 },
+  { id: 13, name: "", description: "", image: Omelete, price: 0 },
+  { id: 14, name: "", description: "", image: Tea, price: 0 },
+  { id: 15, name: "", description: "", image: PavBhaji, price: 0 },
+  { id: 16, name: "", description: "", image: Poori, price: 0 },
+  { id: 17, name: "", description: "", image: PureVeg, price: 0 },
+];
+
+export const images = {
+  caboutbg,
+  Kebabs,
+  Dining,
+  GulabJamun,
+  NorthIndian,
+  Noodles,
+  Lassi,
+  Khichdi,
+  bgimg,
+  Dessert,
+  Tea,
+  Tandoori,
+  Shreeji,
+  Rasgulla,
+  PaneerButter,
+  Biryani,
+  PureVeg,
+  Poori,
+  PavBhaji,
+  OrderOnline,
+  Omelete,
+  Cake,
+  Chinese,
+  CholeBhatore,
+  Coffee,
+  Paratha,
+};
+
+function assests() {
+  return <></>;
+}
+
+
+
+
+export default assests;
